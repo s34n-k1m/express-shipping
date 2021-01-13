2ea1f1fc836ab11d953d7ec34e23fbcf2ad6640d
Sean Kim <sean.m.kim@gmail.com>
2021-01-12 16:19:51 -0800

    Added schema and shipment tests

--- routes/shipments.js
+++ routes/shipments.js
@@ -5,6 +5,10 @@ const router = new express.Router();
 
 const { shipProduct } = require("../shipItApi");
 
+const jsonschema = require("jsonschema");
+const orderSchema = require("../schemas/order.json");
+const {BadRequestError} = require("../expressError");
+
 /** POST /ship
  *
  * VShips an order coming from json body:
@@ -14,9 +18,24 @@ const { shipProduct } = require("../shipItApi");
  */
 
 router.post("/", async function (req, res, next) {
-  const { productId, name, addr, zip } = req.body;
-  const shipId = await shipProduct({ productId, name, addr, zip });
-  return res.json({ shipped: shipId });
+
+  try {
+    const result = jsonschema.validate(req.body, orderSchema);
+    if (!result.valid) {
+      // pass validation errors to error handler
+      //  (the "stack" key is generally the most useful)
+      let errs = result.errors.map(err => err.stack);
+      throw new BadRequestError(errs);
+    }
+    
+    const { productId, name, addr, zip } = req.body;
+    const shipId = await shipProduct({ productId, name, addr, zip });
+    return res.json({ shipped: shipId });
+
+  } catch (err) {
+    return next(err);
+  }
+  
 });
 
 
--- routes/shipments.test.js
+++ routes/shipments.test.js
@@ -15,4 +15,121 @@ describe("POST /", function () {
 
     expect(resp.body).toEqual({ shipped: expect.any(Number) });
   });
+
+  test("invalid product id", async function () {
+    const resp = await request(app).post("/shipments").send({
+      productId: 999,
+      name: "Test Tester",
+      addr: "100 Test St",
+      zip: "12345-6789",
+    });
+
+    expect(resp.statusCode).toEqual(400);
+    expect(resp.body).toEqual({
+      "error": {
+        "message": [
+          "instance.productId must be greater than or equal to 1000"
+        ],
+        "status": 400
+      }
+    });
+  });
+
+  test("invalid zip, less than 5 characters", async function () {
+    const resp = await request(app).post("/shipments").send({
+      productId: 1000,
+      name: "Test Tester",
+      addr: "100 Test St",
+      zip: "1",
+    });
+
+    expect(resp.statusCode).toEqual(400);
+    expect(resp.body).toEqual({
+      "error": {
+        "message": [
+          "instance.zip does not meet minimum length of 5"
+        ],
+        "status": 400
+      }
+    });
+  });
+
+  test("invalid zip, more than 10 characters", async function () {
+    const resp = await request(app).post("/shipments").send({
+      productId: 1000,
+      name: "Test Tester",
+      addr: "100 Test St",
+      zip: "12345-67890000000",
+    });
+
+    expect(resp.statusCode).toEqual(400);
+    expect(resp.body).toEqual({
+      "error": {
+        "message": [
+          "instance.zip does not meet maximum length of 10"
+        ],
+        "status": 400
+      }
+    });
+  });
+
+  test("invalid body passed, invalid data types", async function () {
+    const resp = await request(app).post("/shipments").send({
+      productId: "wrongId",
+      name: 12512,
+      addr: 111111,
+      zip: 123456
+    });
+
+    expect(resp.statusCode).toEqual(400);
+    expect(resp.body).toEqual({
+      "error": {
+        "message": [
+          "instance.productId is not of a type(s) integer",
+          "instance.name is not of a type(s) string",
+          "instance.addr is not of a type(s) string",
+          "instance.zip is not of a type(s) string"
+        ],
+        "status": 400
+      }
+    });
+  });
+
+  test("invalid body passed, missing required property", async function () {
+    const resp = await request(app).post("/shipments").send({
+      productId: 1000,
+      name: "Test Tester",
+      zip: "12345-1234"
+    });
+
+    expect(resp.statusCode).toEqual(400);
+    expect(resp.body).toEqual({
+      "error": {
+        "message": [
+          "instance requires property \"addr\""
+        ],
+        "status": 400
+      }
+    });
+  });
+
+  test("invalid body passed, added extra property", async function () {
+    const resp = await request(app).post("/shipments").send({
+      productId: 1000,
+      name: "Test Tester",
+      addr: "100 Test St",
+      zip: "12345-1234",
+      extraProperty: "I should not be here"
+    });
+
+    expect(resp.statusCode).toEqual(400);
+    expect(resp.body).toEqual({
+      "error": {
+        "message": [
+          "instance is not allowed to have the additional property \"extraProperty\""
+        ],
+        "status": 400
+      }
+    });
+  });
 });
